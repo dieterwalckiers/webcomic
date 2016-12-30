3bcac891cebd9da54538dac953ae7b769245bcbb
dyte <dieter.walckiers@gmail.com>
2016-12-30 23:29:07 +0100

    revert reordering

--- verkrijt.js
+++ verkrijt.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use('/static_verkrijt', express.static(__dirname + '/public'));
 
 app.get('/verkrijt', function(req, res) {
     res.sendFile(__dirname + '/index.html');
