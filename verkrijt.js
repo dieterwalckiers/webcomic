22596581205ae4e742c8b3c45a6e1fc91468721d
dyte <dieter.walckiers@gmail.com>
2016-12-30 21:06:48 +0100

    added welcome string

--- verkrijt.js
+++ verkrijt.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Verkrijt webcomic\n');
+  res.end('Verkrijt webcomic. Welcome.\n');
 }).listen(8081, 'localhost');
 
 console.log('Webcomic running at http://localhost:8081/');
