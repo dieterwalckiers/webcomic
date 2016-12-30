87a5500254774e8d3cf05ef6f60c9c7d3740b03b
dyte <dieter.walckiers@gmail.com>
2016-12-30 23:12:31 +0100

    yea revert that

--- verkrijt.js
+++ verkrijt.js
@@ -4,23 +4,21 @@ var app = express();
 
 app.use('/static_verkrijt', express.static(__dirname + '/public'));
 
-var dirname = __dirname;
-
 app.get('/', function(req, res) {
-    res.sendFild(express.static(__dirname + '/public/index.html'));
-    //res.sendFile(__dirname + '/public/index.html');
+    //res.sendFild(express.static(__dirname + '/public/index.html'));
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-//app.listen(8081);
+app.listen(8081);
 
-var http = require('http');
-
-var server = http.createServer();
-
-server.on('request', function(request, response){
-  response.writeHead(200);
-  response.write("Hello, this is dog, dirname: " + dirname);
-  response.end();
-});
-
-server.listen(8081);
+// var http = require('http');
+//
+// var server = http.createServer();
+//
+// server.on('request', function(request, response){
+//   response.writeHead(200);
+//   response.write("Hello, this is dog, dirname: " + dirname);
+//   response.end();
+// });
+//
+// server.listen(8081);
