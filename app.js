b43d164be1bea4d855dd24e871885567330e8c50
dyte <dieter.walckiers@gmail.com>
2016-12-30 21:00:35 +0100

    setting up

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Verkrijt webcomic\n');
+}).listen(8081, 'localhost');
+
+console.log('Webcomic running at http://localhost:8081/');
