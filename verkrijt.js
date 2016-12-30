a3aaec7a5d479f075e54d75f62eaa1d346bfd98a
dyte <dieter.walckiers@gmail.com>
2016-12-30 23:07:54 +0100

    print dirname

--- verkrijt.js
+++ verkrijt.js
@@ -4,19 +4,23 @@ var app = express();
 
 app.use('/static_verkrijt', express.static(__dirname + '/public'));
 
+var dirname = __dirname;
+
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html'); 
+    res.sendFild(express.static(__dirname + '/public/index.html'));
+    //res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(8081);
-// var http = require('http');
-//
-// var server = http.createServer();
-//
-// server.on('request', function(request, response){
-//   response.writeHead(200);
-//   response.write("Hello, this is dog.");
-//   response.end();
-// });
-//
-// server.listen(8081);
+//app.listen(8081);
+
+var http = require('http');
+
+var server = http.createServer();
+
+server.on('request', function(request, response){
+  response.writeHead(200);
+  response.write("Hello, this is dog, dirname: " + dirname);
+  response.end();
+});
+
+server.listen(8081);
