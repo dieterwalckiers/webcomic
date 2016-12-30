efbd9ac6a5511b096ff183b0c3ae44e80211b712
dyte <dieter.walckiers@gmail.com>
2016-12-30 23:23:34 +0100

    cleanup

--- verkrijt.js
+++ verkrijt.js
@@ -5,21 +5,7 @@ var app = express();
 app.use('/static_verkrijt', express.static(__dirname + '/public'));
 
 app.get('/verkrijt', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-    //res.sendFile(path.join(__dirname, '../public', 'index.html'));
-    //res.sendFile('public/index.html' , { root : __dirname});
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(8081);
-
-// var http = require('http');
-//
-// var server = http.createServer();
-//
-// server.on('request', function(request, response){
-//   response.writeHead(200);
-//   response.write("Hello, this is dog, dirname: " + dirname);
-//   response.end();
-// });
-//
-// server.listen(8081);
